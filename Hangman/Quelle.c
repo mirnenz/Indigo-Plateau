7c6405a37db782933026bfaca9a99688c0558a47
Nastyyy1 <thorben_siering@web.de>
2021-12-19 23:12:57 +0100

    Grobes Menue erstellt

--- Hangman/Quelle.c
+++ Hangman/Quelle.c
@@ -1,6 +1,51 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <conio.h>
+#include "Hangman.h"
+
 
 int main() {
 
-	printf("Hello");
+    int i = 0, Wert = getchar();
+    char* Auswahl = "Start";
+    unsigned char Input;
+
+
+    //Hauptmenü
+
+    StartROT();
+
+    while (1)
+    {
+
+        Input = _getch();
+
+        //80 gleich Pfeil unten
+        //72 gleich Pfeil oben
+
+
+        if (Input == 80 & Auswahl == "Start") {
+            system("cls");
+            OptionenROT();
+            Auswahl = "Optionen";
+        }
+        else if (Input == 72 & Auswahl == "Start") {
+            system("cls");
+            OptionenROT();
+            Auswahl = "Optionen";
+        }
+        else if (Input == 72 & Auswahl == "Optionen") {
+            system("cls");
+            StartROT();
+            Auswahl = "Start";
+        }
+        else if (Input == 80 & Auswahl == "Optionen") {
+            system("cls");
+            StartROT();
+            Auswahl = "Start";
+
+        }
+    }
 }
