f168103d9952c3e32b13131c5c4abae2ec93b084
Mirko Nenzel <mirko.nenzel@informatik.hs-fulda.de>
2021-12-19 21:12:38 +0100

    ASCI Abb. hinzugefügt

---
+++ Hangman.h
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+void Galgen() {
+	printf("_________\n");
+	printf("| /      \n");
+	printf("|/       \n");
+	printf("|        \n");
+	printf("|        \n");
+	printf("|        \n");
+	printf("|________\n");
+}
+void GalgenSeil() {
+	printf("_________\n");
+	printf("| /    | \n");
+	printf("|/       \n");
+	printf("|        \n");
+	printf("|        \n");
+	printf("|        \n");
+	printf("|________\n");
+}
+void GalgenKopf() {
+	printf("_________\n");
+	printf("| /    | \n");
+	printf("|/     O \n");
+	printf("|        \n");
+	printf("|        \n");
+	printf("|        \n");
+	printf("|________\n");
+}
+void GalgenBrust() {
+	printf("_________\n");
+	printf("| /    | \n");
+	printf("|/     O \n");
+	printf("|      | \n");
+	printf("|        \n");
+	printf("|        \n");
+	printf("|________\n");
+}
+void GalgenArmR() {
+	printf("_________\n");
+	printf("| /    | \n");
+	printf("|/     O \n");
+	printf("|      |\\\n");
+	printf("|        \n");
+	printf("|        \n");
+	printf("|________\n");
+}
+void GalgenArmRL() {
+	printf("_________\n");
+	printf("| /    | \n");
+	printf("|/     O \n");
+	printf("|     /|\\\n");
+	printf("|        \n");
+	printf("|        \n");
+	printf("|________\n");
+}
+void GalgenBeinR() {
+	printf("_________\n");
+	printf("| /    | \n");
+	printf("|/     O \n");
+	printf("|     /|\\\n");
+	printf("|       \\\n");
+	printf("|        \n");
+	printf("|________\n");
+}
+void GalgenBeinRL() {
+	printf("_________\n");
+	printf("| /    | \n");
+	printf("|/     O \n");
+	printf("|     /|\\\n");
+	printf("|     / \\\n");
+	printf("|        \n");
+	printf("|________\n");
+}
+void GalgenBeinRL() {
+	printf("######################################################\n");
+	printf("#   #   # #### ###  #       ##    ###  #### ##  #   #\n");
+	printf("#   #   # #    #  # #     ##  ##  #  # #    # # #   #\n");
+	printf("#   #   # #### ###  #    ##    ## ###  #### #  ##   #\n");
+	printf("#    # #  #    # #  #     ##  ##  # #  #    #  ##   #\n");
+	printf("#     #   #### #  # ####    ##    #  # #### #   #   #\n");
+	printf("######################################################\n");
+}
